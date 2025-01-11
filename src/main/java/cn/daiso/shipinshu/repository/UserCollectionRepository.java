2e384dd3728789084e84f280398501f2b93670d3
陈志虎 <Tora_Chen@outlook.com>
2025-01-11 14:20:16 +0800

    添加两个查找函数

--- src/main/java/cn/daiso/shipinshu/repository/UserCollectionRepository.java
+++ src/main/java/cn/daiso/shipinshu/repository/UserCollectionRepository.java
@@ -2,7 +2,9 @@ package cn.daiso.shipinshu.repository;
 
 import cn.daiso.shipinshu.entity.UserCollection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface UserCollectionRepository extends JpaRepository<UserCollection, Long> {
     UserCollection findByUserIdAndLectureId(Long userId, Long lectureId);
+    List<UserCollection> findByUserId(Long userId);
 }
