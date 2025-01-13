94137640c53fb8f5ae1d260735e8d39e27c6e2c6
杨佳林 <15272789+yjlsDreamHouse@user.noreply.gitee.com>
2025-01-13 19:44:03 +0800

    获取推荐课程功能

--- src/main/java/cn/daiso/shipinshu/repository/LectureRepository.java
+++ src/main/java/cn/daiso/shipinshu/repository/LectureRepository.java
@@ -2,10 +2,13 @@ package cn.daiso.shipinshu.repository;
 
 import cn.daiso.shipinshu.entity.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long > {
         List<Lecture> findByIdIn(List<Long> lectureIds);
         List<Lecture> findByUploaderId(Long userId);
+        @Query("SELECT l.id FROM Lecture l")
+        List<Long> findAllIds();   // 获取所有课程
 }
