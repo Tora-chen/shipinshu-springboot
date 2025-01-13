89525702c549ca33f9512a51c2c0fd9e61a32528
陈志虎 <Tora_Chen@outlook.com>
2025-01-13 09:40:50 +0800

    添加Note实体，存储视频截图和描述。

---
+++ src/main/java/cn/daiso/shipinshu/entity/Note.java
@@ -0,0 +1,24 @@
+package cn.daiso.shipinshu.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "note")
+@Data
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="img_id")
+    private Long imgId;
+
+    @Column(name="description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name="video_id")
+    private Long videoId;
+}
